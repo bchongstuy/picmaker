7c2cba4b7b8a3d330791e5568286bdf4c2482f9b
Brandon Chong <33489292+bchongstuy@users.noreply.github.com>
2019-02-02 15:25:17 -0500

    Add files via upload

--- imagemaker.py
+++ imagemaker.py
@@ -1,6 +1,6 @@
 file = open ('image.ppm', 'w')
-file.write ('P3\n500 500\n256')
+file.write ('P3\n500 500\n256\n')
 
 for i in range(500):
     for n in range(500):
-      file.write(i + ' ' + (i + n) + ' ' + (i * n))
+      file.write(str(i % 256) + ' ' + str((i + n) % 256) + ' ' + str((i * n) % 256) + ' ')
